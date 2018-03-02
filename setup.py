1aa0e87414fd060fcd684d2366a1150e743aeb0b
Kevin Beam <kbeam@nsidc.org>
2018-03-02 14:47:14 -0700

    v0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gps-timemachine',
-      version='0.2.1',
+      version='0.2.2',
       description='Adjust GPS datetimes to UTC.',
       url='git@bitbucket.org:nsidc/gps-timemachine.git',
       author='NSIDC Development Team',
