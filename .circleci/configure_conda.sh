7423c6c3c722ab0f98bf5b511682596421b5c522
Trey Stafford <trey.stafford@nsidc.org>
2018-11-13 16:12:55 -0700

    Update configure_conda
    pin conda-build and anaconda-client. Newer versions do not work.

--- .circleci/configure_conda.sh
+++ .circleci/configure_conda.sh
@@ -5,5 +5,5 @@ conda config --add channels conda-forge
 conda config --add channels nsidc
 conda config --add channels nsidc/label/dev
 
-conda install -y "musher>=0.6.3,<0.7.0"
-conda install -y invoke=0.13.0 conda-build anaconda-client
+conda install -y "musher>=0.6.3,<1.0.0a"
+conda install -y "conda-build <3.16.0" "anaconda-client=1.6.7"
