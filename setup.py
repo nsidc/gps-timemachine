29dd49f9f0fb449a4c3c1ad2d067edc597253370
Scott Lewis <scott.lewis@nsidc.org>
2020-12-18 12:58:52 -0700

    "Bump version: 1.0.0 to 1.1.0"

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gps-timemachine',
-      version='1.0.0',
+      version='1.1.0',
       description='Adjust GPS datetimes to UTC.',
       url='git@bitbucket.org:nsidc/gps-timemachine.git',
       author='NSIDC Development Team',
